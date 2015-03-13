4b7332b2bf16b321422b2085220e0b5e9d32e1cd
Willifme <penguinwithwaffle@gmail.com>
2015-03-13 23:27:36 +0000

    Removed zenburn

--- elpa/zenburn-theme-20150308.125/zenburn-theme-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; zenburn-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-<<<<<<< HEAD:elpa/zenburn-theme-20150308.125/zenburn-theme-autoloads.el
-;;;### (autoloads nil "zenburn-theme" "zenburn-theme.el" (21759 8574
-;;;;;;  0 0))
-=======
-;;;### (autoloads nil "zenburn-theme" "zenburn-theme.el" (21763 8272
-;;;;;;  190377 495000))
->>>>>>> origin:elpa/zenburn-theme-20150308.125/zenburn-theme-autoloads.el
-;;; Generated autoloads from zenburn-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-<<<<<<< HEAD:elpa/zenburn-theme-20150308.125/zenburn-theme-autoloads.el
-=======
-;;;### (autoloads nil nil ("zenburn-theme-pkg.el") (21763 8272 206431
-;;;;;;  535000))
-
-;;;***
-
-(provide 'zenburn-theme-autoloads)
->>>>>>> origin:elpa/zenburn-theme-20150308.125/zenburn-theme-autoloads.el
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; zenburn-theme-autoloads.el ends here
--- elpa/zenburn-theme-20150308.125/zenburn-theme-pkg.el
+++
@@ -1,3 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "zenburn-theme" "20150308.125" "A low contrast color theme for Emacs." 'nil :url "http://github.com/bbatsov/zenburn-emacs")
-(define-package "zenburn-theme" "20150308.125" "A low contrast color theme for Emacs." (quote nil))
