36a0d384372543e97f67c914f304f16af4f321d6
Willifme <penguinwithwaffle@gmail.com>
2015-06-27 18:28:26 +0100

    Updated plugins, added tmux config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 cd ~
 
+#git clone https://github.com/gmarik/Vundle.vim.git ~/.dotfiles/.nvim/bundle/Vundle.vim
+
 rm -r .nvimrc
 rm .nvim
 
@@ -9,6 +11,6 @@ ln -s .dotfiles/.nvim ~/.nvim
 cd .dotfiles
 
 #Install youcompleteme
-#cd .nvim/bundle/youcompleteme/
+cd .nvim/bundle/youcompleteme/
 
 #./install.sh --clang-completer
