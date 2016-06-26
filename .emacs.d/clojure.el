8aba7bf512a6d65c6f0a778dc75aaf4d2e2450f3
Willifme <willifme@gmail.com>
2016-06-26 20:28:13 +0100

    Update config

--- .emacs.d/clojure.el
+++
@@ -1,9 +0,0 @@
-(add-hook 'clojure-mode-hook #'enable-paredit-mode)
-(add-hook 'cider-mode-hook #'enable-paredit-mode)
-
-(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
-(add-hook 'cider-mode-hook #'rainbow-delimiters-mode)
-
-;; More sensibile defaults for cider
-(defalias 'cider-start 'cider-jack-in)
-(defalias 'cider-exit 'cider-quit)
